358c604cfff2f7f2d3646f55b0171b10275b4b56
Quinn Gale <q.gale@live.com>
2025-09-24 00:00:30 -0600

    Version 1 Basic time animations and styling

---
+++ script.js
@@ -0,0 +1,32 @@
+let clock = document.querySelector('object');
+
+
+
+clock.addEventListener('load', e => {
+	function spin() {
+		let now = new Date();
+
+		const hours = now.getHours();
+		const minutes = now.getMinutes();
+		const seconds = now.getSeconds();
+
+		const hourRotation = (hours * 30) + ((seconds + minutes * 60) / 3600) * 30;
+		const minuteRotation = minutes * 6 + seconds / 10;
+		const secondRotation = seconds * 6;
+
+		hourHand.style.transform = `rotate(${hourRotation}deg)`;
+		minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
+		secondHand.style.transform = `rotate(${secondRotation}deg)`;
+
+		requestAnimationFrame(spin);
+	}
+
+	const clockDocument = clock.contentDocument;
+	const secondHand = clockDocument.querySelector('#second-hand');
+	const minuteHand = clockDocument.querySelector('#minute-hand');
+	const hourHand = clockDocument.querySelector('#hour-hand');
+	secondHand.style.transformOrigin = '175px 175px';
+	minuteHand.style.transformOrigin = '175px 175px';
+	hourHand.style.transformOrigin = '175px 175px';
+	spin();
+}, false);
